1d95b5192b70472df1209ac8692e0b982d660dbf
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-04-23 15:35:59 -0300

    cria arquivo de utils

---
+++ src/components/utils.js
@@ -0,0 +1,57 @@
+export const SIZE_MYBAR = 80
+// 4e4187-dbebc0-3185fc-20a39e-ef5b5b
+export const COLORS = {
+  primaryFont: '#FFF8F0',
+  secondaryFont: '#BBB',
+  primaryBorder: '#FFF',
+  secondaryBorder: '#CCC',
+  primary: '#3a8287',
+  secondary: '#297176',
+  primaryContrast: '#186065',
+  gray: '#DDD',
+  dark: '#666',
+  red: '#ef5b5b'
+}
+export const PERIODOS = [
+  'Manhã',
+  'Tarde',
+  'Noite',
+]
+
+export const FUNCTIONS_ICONS = {
+  leader: {
+    name: 'Ministração', 
+    icon: 'microphone-alt',
+    unique: true
+  },
+  vocals: {
+    name: 'Vocal', 
+    icon: 'users'
+  },
+  guitars: {
+    name: 'Guitarra', 
+    icon: 'guitar'
+  },
+  strings: {
+    name: 'Violão', 
+    icon: 'guitar'
+  },    
+  drums: {
+    name: 'Bateria', 
+    icon: 'drum'
+  },
+  keyboards: {
+    name: 'Teclado', 
+    icon: 'keyboard'
+  },
+  metals: {
+    name: 'Sopro', 
+    icon: 'user-plus'
+  },
+  bass: {
+    name: 'Baixo', 
+    icon: 'guitar'
+  },
+}
+export const GROUP_NAME = ''
+export const INTERVAL_DATE_CACHE = -10
