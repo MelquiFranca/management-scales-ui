5947d50bc15e8b024c78516aabb172d86b70adc3
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-04-23 15:35:43 -0300

    cria componente view content

---
+++ src/components/ViewContent/index.jsx
@@ -0,0 +1,8 @@
+import * as S from './style.js'
+const ViewContent = ({ children }) => {
+  return <S.ViewContent>
+    {children}
+  </S.ViewContent>
+}
+
+export default ViewContent
---
+++ src/components/ViewContent/style.js
@@ -0,0 +1,8 @@
+'use client'
+import styled from "styled-components"
+import { COLORS } from "../utils"
+
+export const ViewContent = styled.div`
+  background-color: ${COLORS.secondary};
+  flex: 1;
+`
