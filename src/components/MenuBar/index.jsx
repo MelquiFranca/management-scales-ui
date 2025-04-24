39cff63c9376f91370359fa2d7c5fb29ab336b16
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-04-24 13:28:48 -0300

    cria componente menu bar

---
+++ src/components/MenuBar/index.jsx
@@ -0,0 +1,10 @@
+import * as S from './style'
+const MenuBar = ({ children }) => {
+  return <S.Container>
+    <S.Button>Escalas</S.Button>
+    <S.Button>Membros</S.Button>
+    <S.Button>Eventos</S.Button>
+  </S.Container>
+}
+
+export default MenuBar
---
+++ src/components/MenuBar/style.js
@@ -0,0 +1,25 @@
+'use client'
+import styled from "styled-components"
+
+export const Container = styled.div`
+  flex: 1;
+  position: fixed;
+  bottom: 0;
+  width: 100dvw;
+  padding: 10px;
+  gap: 2px;
+`
+export const Button = styled.button`
+  flex: 1;
+  padding: 10px;
+  background: none;
+  border: none;
+  color: ${({ theme }) => theme.primaryFont};
+  font-size: 1rem;
+  display: flex;
+  flex-direction: column;
+`
+export const ButtonIcon = styled.button`
+  flex: 1;
+  padding: 10px;
+`
