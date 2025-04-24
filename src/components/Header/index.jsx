88816b209f9653dc6c3a613c250bed21feb8019e
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-04-24 14:02:14 -0300

    cria componente header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import * as S from './style'
+
+export default function Header ({ groups, user }) {
+  return <S.Container>
+    <S.ListGroups>
+      {groups.map((group, id) => <S.GroupNameItem id={id}>{group.name}</S.GroupNameItem>)}
+    </S.ListGroups>
+    <S.Avatar>
+      <S.Image src={user.image}></S.Image>
+    </S.Avatar>
+  </S.Container>
+}
---
+++ src/components/Header/style.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 60px;
+  position: fixed:
+  top: 0;
+  align-items: center;
+  gap: 10px;
+`
+export const ListGroups = styled.select`
+  flex: 1;
+  background: none;
+  padding: 5px 0;
+  color: ${({ theme }) => theme.primaryFont};
+  font-size: 1.4rem;
+  font-weight: bold;
+  border: none;
+`
+export const GroupNameItem = styled.option`
+`
+export const Image = styled.img`
+  flex: 1;
+  height: 100%;
+  width: 100%;
+  border-radius: 50%;
+`
+export const Avatar = styled.button`
+  height: 50px;
+  width: 50px;
+  border: none;
+  background: none;
+`
+
