cb8b0eb81f807f4c230a513689c5f17f2def093d
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-04-29 10:49:58 -0300

    cria componente calendar

---
+++ src/components/Calendar/index.jsx
@@ -0,0 +1,151 @@
+'use client'
+import { useEffect, useState } from 'react'
+import Modal from '../Modal'
+import * as S from './style'
+
+Date.prototype.getDaysOfMonth = (year, month, day) => {
+  const date = (year && month && day) ? new Date(year, month, day) : new Date()
+  if (date.getMonth() === 1) {
+    const year = date.getFullYear()
+    const days = !(year % 4) ? ((year % 400) ? 29 : 28) : !(year % 100) ? 29 : 28
+    return days
+  }
+  const actualMonth = date.getMonth()
+  date.setDate(31)
+  const days = (date.getMonth() === actualMonth) ? 31 : 30
+  return days
+}
+
+export default function Calendar ({ handleDate, visible, setVisible }) {
+  const [date, setDate] = useState(new Date())
+  const [selectedDay, setSelectedDay] = useState(date.getDate())
+  const [selectedMonth, setSelectedMonth] = useState(date.getMonth())
+  const [selectedMonthName, setSelectedMonthName] = useState()
+  const [selectedYear, setSelectedYear] = useState(date.getFullYear())
+  const [quantityDaysOfMonth, setQuantityDaysOfMonth] = useState(date.getDaysOfMonth())
+  const getCalendarValues = () => ([
+    ...Array(date.getDay()).fill(''),
+    ...Array
+      .from(Array(quantityDaysOfMonth))
+      .map((_, index) => index + 1),
+  ])
+  const [calendarValues, setCalendarValues] = useState(getCalendarValues())
+  
+  const getMonthName = (actualDate) => Intl.DateTimeFormat(
+    'pt-BR',
+    { month: 'long' }
+  ).format(actualDate)
+  const handleClickConfirm = () => {
+    handleDate(new Date(selectedYear, selectedMonth, selectedDay))
+    setVisible(false)
+  }
+  const handleClickCancel = () => {
+    setVisible(false)
+  }
+  const handleClickCalendarDay = (day) => setSelectedDay(day)
+  const handleClickPrevMonth = () => {
+    setDate(new Date(selectedYear, selectedMonth > 0 ? selectedMonth - 1 : 11, 1))
+    setSelectedMonth(selectedMonth > 0 ? selectedMonth - 1 : 11)
+    setSelectedDay(1)
+  }
+  const handleClickNextMonth = () => {
+    setDate(new Date(selectedYear, selectedMonth + 1, 1))
+    setSelectedMonth(selectedMonth + 1)
+    setSelectedDay(1)
+  }
+  const handleClickPrevYear = () => {
+    setDate(new Date(selectedYear - 1, selectedMonth, 1))
+    setSelectedYear(selectedYear - 1)
+    setSelectedDay(1)
+  }
+  const handleClickNextYear = () => {
+    setDate(new Date(selectedYear + 1, selectedMonth, 1))
+    setSelectedYear(selectedYear + 1)
+    setSelectedDay(1)
+  }
+  useEffect(() => {
+    const days = date.getDaysOfMonth(selectedYear, selectedMonth, 1)
+    setSelectedMonthName(getMonthName(new Date(selectedYear, selectedMonth, selectedDay)))
+    setQuantityDaysOfMonth(days)
+  }, [selectedDay, selectedMonth, selectedYear])
+  useEffect(() => setCalendarValues(getCalendarValues()), [date, quantityDaysOfMonth])
+  const Item = ({ item }) => (<S.DayItem
+    // disabled={!item}
+    key={item}
+    onClick={() => handleClickCalendarDay(item)}
+  >{item}</S.DayItem>)
+  return (
+    <Modal
+      visible={visible}
+    >
+      <S.Container>
+        <S.Calendar>
+          <S.Content>
+            <S.Filter>
+              <S.FilterFieldMonth>
+                <S.FilterButton
+                 
+                  onClick={handleClickPrevMonth}
+                >
+                  {/* <Icons name='chevron-left'/> */}
+                </S.FilterButton>
+                {selectedMonthName}
+                <S.FilterButton
+                  onClick={handleClickNextMonth}
+                >
+                  {/* <Icons name='chevron-right'/> */}
+                </S.FilterButton>
+              </S.FilterFieldMonth>
+              <S.FilterFieldYear>
+              <S.FilterButton
+               
+                onClick={handleClickPrevYear}
+              >
+                  {/* <Icons name='chevron-left'/> */}
+                </S.FilterButton>
+                {selectedYear}
+                <S.FilterButton
+                 
+                  onClick={handleClickNextYear}
+                >
+                  {/* <Icons name='chevron-right'/> */}
+                </S.FilterButton>
+              </S.FilterFieldYear>
+            </S.Filter>
+            <S.Header>
+              {
+                ['D','S','T','Q','Q','S', 'S']
+                  .map(
+                    (day, index) => <S.HeaderTitle key={index}>{day}</S.HeaderTitle>
+                  )
+              }
+            </S.Header>
+            <S.CalendarContent>
+              {/* <FlatList
+                numColumns={7}
+                centerContent={true}
+                data={}
+                renderItem={Item}
+              /> */}
+              {calendarValues.map((data) => <Item item={data}/>)}
+            </S.CalendarContent>
+          </S.Content>
+        </S.Calendar>
+        <S.Actions>
+          <S.ActionButtonConfirm
+           
+            onClick={handleClickConfirm}
+          >
+            Confirmar
+          </S.ActionButtonConfirm>
+          <S.ActionButtonCancel
+           
+            onClick={handleClickCancel}
+          >
+            Cancelar
+          </S.ActionButtonCancel>
+        </S.Actions>
+      </S.Container>
+    </Modal>
+  )
+}
---
+++ src/components/Calendar/style.js
@@ -0,0 +1,113 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  // background-color: ${({ theme }) => theme.primary};
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 5px;
+  flex-direction: column;
+`
+export const Calendar = styled.div`
+  flex: 8;
+  justify-content: center;
+  // min-height: 70%;
+  // max-height: 80%;
+  // maxWidth: 100%;
+  // width: 100%;
+  flex-direction: column;
+`
+export const Content = styled.div`
+  gap: 2px;
+  align-self: center;
+  flex-direction: column;
+  padding-bottom: 20px;
+  border: 2px solid ${({ theme }) => theme.primary};
+`
+export const Actions = styled.div`
+  flex: 1;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  gap: 60px;
+`
+export const ActionButtonConfirm = styled.div`
+  background-color: ${({ theme }) => theme.red};
+  padding: 10px 20px;
+  color: ${({ theme }) => theme.primaryFont};
+  justify-content: center;
+  flex: 1;
+`
+export const ActionButtonCancel = styled.div`
+  background-color: ${({ theme }) => theme.primary};
+  padding: 10px 20px;
+  color: ${({ theme }) => theme.primaryFont};
+  justify-content: center;
+  flex: 1;
+`
+export const Filter = styled.div`
+  flex-direction: row;
+  background-color: ${({ theme }) => theme.gray};
+  color: ${({ theme }) => theme.primaryContrast};
+`
+export const FilterFieldMonth = styled.div`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  padding: 20px 5px;
+`
+export const FilterFieldYear = styled.div`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px 5px;
+  gap: 5px;
+`
+export const FilterButton = styled.div`
+  padding: 10px 20px;
+`
+export const CalendarContent = styled.div`
+  display: grid;
+  background-color: ${({ theme }) => theme.secondary};
+  grid-template: 
+    "sun1 mon1 tue1 wed1 thu1 fri1 sat1" 1fr
+    "sun2 mon2 tue2 wed2 thu2 fri2 sat2" 1fr
+    "sun3 mon3 tue3 wed3 thu3 fri3 sat3" 1fr
+    "sun4 mon4 tue4 wed4 thu4 fri4 sat4" 1fr;
+  padding: 0 5px;
+  gap: 0px;
+`
+export const Header = styled.div`
+  flex-direction: row;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.gray};
+  align-items: center;
+`
+export const HeaderTitle = styled.div`
+  padding: 10px;
+  flex: 1;
+  fontWeight: bold;
+  color: ${({ theme }) => theme.primaryContrast};
+`
+export const DayItem = styled.div`
+  background-color: ${({ theme }) => theme.secondary};
+  width: 42px;
+  height: 42px;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  margin: 6px 2px;
+  border-width: 0.5;
+  border: 1px solid ${({ theme }) => theme.primary};
+`
+export const DayNull = styled.div`
+  background-color: ${({ theme }) => theme.secondary};
+  width: 42px;
+  height: 42px;
+  margin: 6px 2px;
+`
+export const SelectedDay = styled.div`
+  background-color: ${({ theme }) => theme.red};
+}`
