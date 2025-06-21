2a9df3a04060c1da045373d2d653d14ba38a3347
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-06-21 13:13:44 -0300

    implementa comunicacao com api

---
+++ src/commons/api.js
@@ -0,0 +1,92 @@
+import { saveToken } from "./authentication"
+
+const HOST = process.env.APP_HOST || 'http://localhost:8002/'
+const TOKEN_TEST = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODFhNTRhMGI1ZWM0NGVhMGI5MjE2MTQiLCJuYW1lIjoiTWVscXVpIFAuIEZyYW5jYSIsInVzZXJuYW1lIjoiZnJhbmNhIiwidHlwZSI6InVzZXIiLCJncm91cElkIjoiNjA3YzcyNGEwMTcxNTkwMDE1ZmY5Yzk0IiwiaWF0IjoxNzQ4NzAwMjE1LCJleHAiOjIxMDg3MDAyMTV9.wJKHiWCa_ZSvY76_wypj0rEDQ3MMzwczi4IyAi8DlZ0'
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${TOKEN_TEST}`
+}
+
+export const getMembers = async () => {
+  const result = await fetch(`${HOST}members`, {
+    method: 'GET',
+    headers,
+    cache: 'force-cache',
+    next: {
+      revalidate: 5//(60 * 60 * 24) * 5 // 5 days
+    }
+  })
+  const { members } = await result.json()
+  return members
+}
+export const putMember = async (id, body) => {
+  const result = await fetch(`${HOST}members/${id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      // 'Content-Type': 'multipart/form-data'
+    },
+    body: JSON.stringify(body)
+  })
+  const data = await result.json()
+  return data
+}
+export const login = async (username, password) => {
+  const result = await fetch(`${HOST}login`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ username, password })
+  })
+  const token = await result.json()
+  saveToken(token)
+  return token
+}
+// export const postCult = async (data) => {
+//   const result = await connection.post('cultos', data)
+//   return result
+// }
+// export const updateUnavaliableMembers = async ({ memberId, unavaliableCultIds }) => {
+//   const result = await connection.post(`cultos/membros/indisponiveis`, {
+//     pessoaId: memberId,
+//     cultosIndisponiveis: unavaliableCultIds
+//   })
+//   return result
+// }
+// export const postScale = async (data) => {
+//   const result = await connection.post('escalas', data)
+//   return result
+// }
+// export const updateAnnotationScale = async (data) => {
+//   const result = await connection.post(`escalas/anotacoes/${data._id}`, data)
+//   return result
+// }
+// export const getCults = async (groupId) => {
+//   const { data } = await connection.get(`cultos/${groupId}`)
+//   return data || []
+// }
+// export const getScales = async (groupId) => {
+//   const { data } = await connection.get(`escalas/${groupId}`)
+//   return data || []
+// }
+// export const deleteScale = async (id) => {
+//   const { data } = await connection.post(`escalas/excluir/${id}`)
+//   return data
+// }
+// export const deleteMembers = async (id) => {
+//   const { data } = await connection.post(`pessoas/excluir/${id}`)
+//   return data
+// }
+// export const deleteCults = async (id) => {
+//   const { data } = await connection.post(`cultos/excluir/${id}`)
+//   return data
+// }
+// export const pushNotification = async (subscription) => {
+//   await connection.post('notification/push/send', {
+//     subscription,
+//     dataMessage: {
+//       title: 'Escala Musical',
+//       body: 'Nova escala criada',
+//       image: './favicon-32.png'
+//     }
+//   })
+// }
