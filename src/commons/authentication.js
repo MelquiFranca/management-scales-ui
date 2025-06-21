ca501cf83696195896bb7e0b74c1815ef76532c4
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-06-21 13:14:28 -0300

    implementa armazenamento de token de autenticacao

---
+++ src/commons/authentication.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+
+export const getTokenData = () => {
+  const token = localStorage.getItem('management-squad-token')
+  return jwt.decode(token, { complete: true })
+}
+export const verifyLocalTokenValidation = async () => {
+  try {
+    const token = localStorage.getItem('management-squad-token')
+    if(!token) return false
+    return true
+  } catch (err) {
+    console.log(err.message)
+    return false
+  }
+}
+export const saveToken = (token) => {
+  localStorage.setItem('management-squad-token', token)
+}
