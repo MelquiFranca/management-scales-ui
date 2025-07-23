b6023ae5c2332abc3a06432e8d07d26d08db05ce
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-07-23 19:29:52 -0300

    adiciona logica no componente header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,11 +2,12 @@
 import Avatar from '../Avatar'
 import * as S from './style'
 
-export default function Header ({ groups, user }) {
+export default function Header ({ groups, activatedGroupId }) {
+  const activatedGroup = groups.get(activatedGroupId)
   return <S.Container>
     <S.ListGroups>
-      {groups.map((group, id) => <S.GroupNameItem key={id}>{group.name}</S.GroupNameItem>)}
+      {Array.from(groups.values()).map((group, id) => <S.GroupNameItem key={id}>{group.name}</S.GroupNameItem>)}
     </S.ListGroups>
-    <Avatar image={user.image} />
+    <Avatar image={activatedGroup?.image} />
   </S.Container>
 }
