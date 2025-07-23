cddaa5b59f4d5adabb369a1e62b52f11afb65e92
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-07-23 19:31:56 -0300

    implementa consumo da api de membros

--- src/app/(home)/members/members.js
+++ src/app/(home)/members/members.js
@@ -1,28 +1,18 @@
 'use client'
-import { use, useContext, useEffect } from 'react'
 import Member from '@/components/Member'
 import ViewContent from '@/components/ViewContent'
 import * as S from './style'
 import { redirect } from 'next/navigation'
-import { DataProvider } from '@/contexts'
 
 export default function Members ({ members }) {
-  const allMembers = use(members)
-  const { setMembers, members: membersContext } = useContext(DataProvider)
-  const handleClick = (memberIndex) => {
-    redirect(`/members/${allMembers[memberIndex]._id}`)
+  const handleClick = (memberId) => {
+    redirect(`/members/${memberId}`)
   }
-  useEffect(() => {
-    if (!membersContext.size) {
-      const membersMap = new Map(allMembers.map(member => [member._id, member]))
-      setMembers(membersMap)
-    }
-  }, [membersContext, setMembers, allMembers])
   return (<ViewContent>
     <S.Container>
-    {[...membersContext.values()]?.map((member, index) =>
+    {Array.from(members.values()).map((member, index) =>
       <S.WrapperMember key={index}>
-        <Member data={member} handleClick={() => handleClick(index)}/>
+        <Member data={member} handleClick={() => handleClick(member._id)}/>
       </S.WrapperMember>
     )}
     </S.Container>
--- src/app/(home)/members/page.js
+++ src/app/(home)/members/page.js
@@ -1,11 +1,12 @@
+'use client'
 import { Suspense } from 'react'
 import Members from './members'
 import Loading from './loading'
-import { getMembers } from '@/commons/api'
+import { useContext } from 'react'
+import { DataProvider } from '@/contexts'
 
 export default function Page () {
-  const members = getMembers()
-  
+  const { members } = useContext(DataProvider)
   return (<Suspense fallback={Loading()}>
     <Members members={members} />
   </Suspense>)
